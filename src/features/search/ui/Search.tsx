7b7ae7ce25d07e1829fcdde83b6ba83da903e958
xoxlushka1337 <ulana.xoxlushka@gmail.com>
2024-01-26 19:15:54 +0300

    feat: add search

---
+++ src/features/search/ui/Search.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const InputContainer = styled.label`
+  background-color: #277322;
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
+  border-radius: var(--radii);
+  width: 100%;
+  margin-bottom: 1.5rem;
+
+  @media (min-width: 767px) {
+    margin-bottom: 0;
+    width: 280px;
+  }
+`
+const Input = styled.input.attrs({
+  type: 'search',
+  placeholder: 'Filter by name...',
+})`
+  margin-left: 2rem;
+  border: none;
+  outline: none;
+  background-color: #18324342;
+  color: white;
+`
+
+const Search = () => {
+  const [valueSearch, setValueSearch] = useState<string>('')
+
+  return (
+    <InputContainer>
+      <Input
+        onChange={(e) => setValueSearch(e.target.value)}
+        value={valueSearch}
+      ></Input>
+    </InputContainer>
+  )
+}
+
+export default Search
---
+++ src/widgets/conrols/Controls.tsx
@@ -0,0 +1,11 @@
+import { Search } from 'features/search'
+
+const Controls = () => {
+  return (
+    <div className="">
+      <Search />
+    </div>
+  )
+}
+
+export default Controls
