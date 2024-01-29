3e0299f261bb75a6b50251ad543a139ac648090f
xoxlushka1337 <ulana.xoxlushka@gmail.com>
2024-01-29 03:27:11 +0300

    refactor: Home Pages

--- src/pages/homePages/HomePages.tsx
+++ src/pages/homePages/HomePages.tsx
@@ -1,10 +1,11 @@
 import { Search } from 'features/search'
 import React, { useEffect, useState } from 'react'
 import { Container } from 'shared/ui'
-import styled from 'styled-components'
 import { Controls } from 'widgets/controls'
 import { Header } from 'widgets/header'
 import { ListCards } from 'widgets/listCards'
+
+import { Wrapper, ShowFiltersButton, ContainerFilter } from './homePages.styles'
 
 interface ITypeFilters {
   name?: string
@@ -13,41 +14,6 @@ interface ITypeFilters {
   type?: string
   species?: string
 }
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 50px;
-`
-
-const ShowFiltersButton = styled.button`
-  font-size: var(--fs-main);
-  padding: 8px 16px;
-  width: 150px;
-  height: 50px;
-  background-color: var(--background-accent);
-  color: white;
-  border: none;
-  border-radius: var(--radii);
-  box-shadow: var(--shadow);
-  letter-spacing: 0.5px;
-  cursor: pointer;
-`
-
-const ContainerFilter = styled.div`
-  display: flex;
-  justify-content: space-around;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px 0px;
-  width: 100%;
-  margin-top: 70px;
-
-  @media (min-width: 512px) {
-    flex-direction: row;
-    gap: 0px;
-  }
-`
 
 const HomePages = () => {
   const [filters, setFilters] = useState<ITypeFilters>({})
